6ef985653207b45f14c26b0391addd3f9f8970b8
ggill <ggill@GARY-HP>
2016-12-12 14:40:02 -0500

    Add project files.

---
+++ MachineBackupInfo/Classes/Statistics.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace MachineBackupInfo.Classes
+{
+    class Statistics
+    {
+        private readonly string BackupDirectory = @"\\fileserver\public storage canada\public\machine backups";
+       
+        //Number of Backups(determined by folder count in directory)
+        public int BackupTotal { get; set; }
+        
+        //Property Directory Count
+        public int PropertyCount { get; set; }
+       
+        //User Directory Count
+        public int UserBackups { get; set; }
+        
+        //Properties with backups
+        public int PropertiesWithBackups { get; set; }
+       
+        //Properties without backups
+        public int PropertiesWithNoBackups { get; set; }
+        
+        //Users with backups
+        public int UsersWithBackups { get; set; }
+
+        //Users without backups
+        public int UsersWithoutBackups { get; set; }
+
+        public Statistics()
+        {
+            BackupTotal = DirectoryCount(BackupDirectory);
+        }
+        
+        //Gets number of Directories in specified path
+        public int DirectoryCount(string path)
+        {
+            //Returns Directory Path or 0 if path does not exist
+            return Directory.Exists(path) ? Directory.GetDirectories(path).Count() : 0;
+        }
+        
+        //Gets number of Directories that are for properties
+        public int PropertyDirectoryCount(string path)
+        {
+            return 
+        }
+    }
+}
+
+        
+
---
+++ MachineBackupInfo/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MahApps.Metro.Controls;
+using MachineBackupInfo.Classes;
+
+namespace MachineBackupInfo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        Statistics stats;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            stats = new Statistics();
+            DataContext = stats;
+        }
+    }
+}
